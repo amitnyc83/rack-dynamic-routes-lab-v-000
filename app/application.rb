d019e8e982fb3276908f7e52a8e2307c6c5ae1a2
Amit Singh <amit_nyc@live.com>
2018-09-12 19:23:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,14 +16,12 @@ class Application
       else
         resp.write "Item not found"
         resp.status = 400
+      end
 
     else
         resp.write "Route not found"
         resp.status = 404
 
-      end
 
-    end
-        resp.finish
-      end
+
     end
